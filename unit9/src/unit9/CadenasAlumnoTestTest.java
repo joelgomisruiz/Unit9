f9edebd987b37964bfd210a4ffa59905d6aa257f
joel <joelgomisruiz@gmail.com>
2015-05-06 20:21:21 +0200

    test del metodo soniguales completados

--- unit9/src/unit9/CadenasAlumnoTestTest.java
+++ unit9/src/unit9/CadenasAlumnoTestTest.java
@@ -7,17 +7,31 @@ import org.junit.Test;
 public class CadenasAlumnoTestTest {
 
 	@Test
-	public void testSonIguales() {
-		String a = "pepe", b = "pepe", c = "pep";
-		assertEquals(true, new CadenasAlumno().sonIguales(a, b));
-		assertEquals(true, new CadenasAlumno().sonIguales(b, a));
-		assertEquals(false, new CadenasAlumno().sonIguales(c, a));
-		assertEquals(false, new CadenasAlumno().sonIguales(a, c));
+	public void testSonIgualesA() {
+		String cadena1 = "entornos", cadena2 = "ento";
+		boolean correcto = cadena1.equals(cadena2);
+		assertEquals(correcto,new CadenasAlumno().sonIguales(cadena1, cadena2));
 	}
 	
 	@Test
-	public void TestSoniguales(){
-		
+	public void TestSonigualesB(){
+		String cadena1 = "ento", cadena2 = "entornos";
+		boolean correcto = cadena1.equals(cadena2);
+		assertEquals(correcto,new CadenasAlumno().sonIguales(cadena1, cadena2));
 	}
+	
+	@Test
+	public void TestSonigualesC(){
+		String cadena1 = "entornos", cadena2 = "sonrotne";
+		boolean correcto = cadena1.equals(cadena2);
+		assertEquals(true,new CadenasAlumno().sonIguales(cadena1, cadena2));
+	}
+	@Test
+	public void TestSonigualesD(){
+		String cadena1 = "entornos", cadena2 = "entornos";
+		boolean correcto = cadena1.equals(cadena2);
+		assertEquals(correcto,new CadenasAlumno().sonIguales(cadena1, cadena2));
+	}
+
 
 }
