8c533417255c3a3e6c32b451e955b0db2166a1a9
joel <joelgomisruiz@gmail.com>
2015-05-07 10:08:52 +0200

    Test de los metodos es mayor, es menor y ignorando mayusculas completado

--- unit9/src/unit9/CadenasAlumnoTestTest.java
+++ unit9/src/unit9/CadenasAlumnoTestTest.java
@@ -9,29 +9,85 @@ public class CadenasAlumnoTestTest {
 	@Test
 	public void testSonIgualesA() {
 		String cadena1 = "entornos", cadena2 = "ento";
-		boolean correcto = cadena1.equals(cadena2);
-		assertEquals(correcto,new CadenasAlumno().sonIguales(cadena1, cadena2));
+		assertEquals(cadena1.equals(cadena2),new CadenasAlumno().sonIguales(cadena1, cadena2));
 	}
 	
 	@Test
 	public void TestSonigualesB(){
 		String cadena1 = "ento", cadena2 = "entornos";
-		boolean correcto = cadena1.equals(cadena2);
-		assertEquals(correcto,new CadenasAlumno().sonIguales(cadena1, cadena2));
+		assertEquals(cadena1.equals(cadena2),new CadenasAlumno().sonIguales(cadena1, cadena2));
 	}
 	
 	@Test
 	public void TestSonigualesC(){
 		String cadena1 = "entornos", cadena2 = "sonrotne";
-		boolean correcto = cadena1.equals(cadena2);
-		assertEquals(true,new CadenasAlumno().sonIguales(cadena1, cadena2));
+		assertEquals(cadena1.equals(cadena2),new CadenasAlumno().sonIguales(cadena1, cadena2));
 	}
 	@Test
 	public void TestSonigualesD(){
 		String cadena1 = "entornos", cadena2 = "entornos";
-		boolean correcto = cadena1.equals(cadena2);
-		assertEquals(correcto,new CadenasAlumno().sonIguales(cadena1, cadena2));
+		assertEquals(cadena1.equals(cadena2),new CadenasAlumno().sonIguales(cadena1, cadena2));
 	}
-
+	@Test
+	public void TestEsMayorA(){
+		String cadena1 = "entornos", cadena2 = "ento";
+		assertEquals(cadena1.compareTo(cadena2) > 0,new CadenasAlumno().esMayor(cadena1, cadena2));
+	}
+	@Test
+	public void TestEsMayorB(){
+		String cadena1 = "ento", cadena2 = "entornos";
+		assertEquals(cadena1.compareTo(cadena2) > 0,new CadenasAlumno().esMayor(cadena1, cadena2));
+	}
+	@Test
+	public void TestEsMayorC(){
+		String cadena1 = "entornos", cadena2 = "sonrotne";
+		assertEquals(cadena1.compareTo(cadena2) > 0,new CadenasAlumno().esMayor(cadena1, cadena2));
+	}
+	@Test
+	public void TestEsMayorD(){
+		String cadena1 = "entornos", cadena2 = "entornos";
+		assertEquals(cadena1.compareTo(cadena2) > 0,new CadenasAlumno().esMayor(cadena1, cadena2));
+	}
+	@Test
+	public void TestEsMenorA(){
+		String cadena1 = "entornos", cadena2 = "ento";
+		assertEquals(cadena1.compareTo(cadena2) < 0,new CadenasAlumno().esMenor(cadena1, cadena2));
+	}
+	@Test
+	public void TestEsMenorB(){
+		String cadena1 = "ento", cadena2 = "entornos";
+		assertEquals(cadena1.compareTo(cadena2) < 0,new CadenasAlumno().esMenor(cadena1, cadena2));
+	}
+	@Test
+	public void TestEsMenorC(){
+		String cadena1 = "entornos", cadena2 = "sonrotne";
+		assertEquals(cadena1.compareTo(cadena2) < 0,new CadenasAlumno().esMenor(cadena1, cadena2));
+	}
+	@Test
+	public void TestEsMenorD(){
+		String cadena1 = "entornos", cadena2 = "entornos";
+		assertEquals(cadena1.compareTo(cadena2) < 0,new CadenasAlumno().esMenor(cadena1, cadena2));
+	}
+	@Test
+	public void TestComparaIgnorandoMayusculasA(){
+		String cadena1 = "Entornos", cadena2 = "ento";
+		assertEquals(cadena1.equalsIgnoreCase(cadena2),new CadenasAlumno().comparaIgnorandoMayusculas(cadena1, cadena2));
+	}
+	@Test
+	public void TestComparaIgnorandoMayusculasB(){
+		String cadena1 = "ento", cadena2 = "entornos";
+		assertEquals(cadena1.equalsIgnoreCase(cadena2),new CadenasAlumno().comparaIgnorandoMayusculas(cadena1, cadena2));
+	}
+	@Test
+	public void TestComparaIgnorandoMayusculasC(){
+		String cadena1 = "enTornos", cadena2 = "sonrotne";
+		assertEquals(cadena1.equalsIgnoreCase(cadena2),new CadenasAlumno().comparaIgnorandoMayusculas(cadena1, cadena2));
+	}
+	@Test
+	public void TestComparaIgnorandoMayusculasD(){
+		String cadena1 = "entornos", cadena2 = "Entornos";
+		assertEquals(cadena1.equalsIgnoreCase(cadena2),new CadenasAlumno().comparaIgnorandoMayusculas(cadena1, cadena2));
+	}
+	
 
 }
